31616995bf2065c451774388666a70f965b7295a
epost <25180070+erpost@users.noreply.github.com>
2018-10-05 13:30:52 -0400

    update

--- error-handling/exceptions.py
+++ error-handling/exceptions.py
@@ -3,3 +3,9 @@ from pprint import pprint
 
 client = boto3.client('s3')
 pprint(client.exceptions.__dict__)
+
+client = boto3.client('iam')
+pprint(client.exceptions.__dict__)
+
+client = boto3.client('sts')
+pprint(client.exceptions.__dict__)
