e6a7da5b5ebbb3fe7958a064cad6c26687364cec
epost <25180070+erpost@users.noreply.github.com>
2018-12-03 10:50:53 -0500

    update

--- ec2/ec2_instances_list-resource.py
+++ ec2/ec2_instances_list-resource.py
@@ -1,24 +1,51 @@
 import boto3
+import csv
 
 
-def get_regions():
-    client = boto3.client('ec2')
-    regions = []
-    response = client.describe_regions()
-    for region in response['Regions']:
-        regions.append(region['RegionName'])
-    return regions
+outfile = 'prod-instances.csv'
+
+with open(outfile, 'w', newline='') as outfile:
+    out_file = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    out_file.writerow(['Instance ID'] + ['Region'] + ['Instance State'] +
+                      ['Private DNS'] + ['Public DNS'] + ['Environment'])
 
 
-for aws_region in get_regions():
-    ec2 = boto3.resource('ec2', region_name=aws_region)
-    print('#' * 10, aws_region, '#' * 10)
-    for instance in ec2.instances.all():
-        print(instance.id, ':', instance.state['Name'])
-        if instance.tags is None:
-            print('No Tags')
-        else:
-            tag_dict = {}
-            for tags in instance.tags:
-                tag_dict[tags['Key']] = tags['Value']
-            print(tag_dict)
+    def get_regions():
+        client = boto3.client('ec2')
+        regions = []
+        response = client.describe_regions()
+        for region in response['Regions']:
+            regions.append(region['RegionName'])
+        return regions
+
+
+    for aws_region in get_regions():
+        ec2 = boto3.resource('ec2', region_name=aws_region)
+        for instance in ec2.instances.all():
+            print('Instance ID: ', instance.id)
+            print('Region: ', aws_region)
+            print('Instance State: ', instance.state['Name'])
+            try:
+                print('Private DNS: ', instance.private_dns_name)
+                priv_dns = instance.private_dns_name
+            except AttributeError:
+                print('Private DNS: None')
+                priv_dns = 'None'
+            try:
+                print('Public DNS: ', instance.public_dns)
+                pub_dns = instance.public_dns
+            except AttributeError:
+                print('Public DNS: None')
+                pub_dns = 'None'
+            if instance.tags is None:
+                print('No Tags')
+            else:
+                tag_dict = {}
+                for tags in instance.tags:
+                    env = 'None'
+                    if tags['Key'] == 'Environment':
+                        print('Environment Tag: ', tags['Value'])
+                        env = tags['Value']
+                    tag_dict[tags['Key']] = tags['Value']
+                # print(tag_dict)
+            out_file.writerow([instance.id] + [aws_region] + [instance.state['Name']] + [priv_dns] + [pub_dns] + [env])
