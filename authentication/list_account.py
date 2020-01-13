98664ccc3edc8ab543a3932082041e1aba039e40
erpost <25180070+erpost@users.noreply.github.com>
2020-01-13 13:22:39 -0500

    commit

---
+++ authentication/list_account.py
@@ -0,0 +1,9 @@
+from pprint import pprint
+import boto3
+
+profiles = boto3.session.Session().available_profiles
+boto3.setup_default_session(profile_name=profiles[0], region_name='us-east-1')
+sts_client = boto3.client('sts')
+
+account_id = sts_client.get_caller_identity()["Account"]
+pprint(account_id)
