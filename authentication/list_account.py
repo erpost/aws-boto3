221fd647414f5f9eeeb6add1b8b6cce25f1498f4
erpost <25180070+erpost@users.noreply.github.com>
2020-12-14 14:23:27 -0500

    commit

--- authentication/list_account.py
+++ authentication/list_account.py
@@ -1,9 +1,10 @@
 from pprint import pprint
 import boto3
 
-profiles = boto3.session.session().available_profiles
+profiles = boto3.session.Session().available_profiles
 boto3.setup_default_session(profile_name=profiles[0], region_name='us-east-1')
 sts_client = boto3.client('sts')
 
-account_id = sts_client.get_caller_identity()["account"]
+pprint(sts_client.get_caller_identity())
+account_id = sts_client.get_caller_identity()["Account"]
 pprint(account_id)
