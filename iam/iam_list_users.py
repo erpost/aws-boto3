4b17c74341eec972cdb89c45bbfcff6ef8f8ba75
Eric Post <eric.post@sapns2.com>
2022-03-31 15:53:55 -0400

    added iam list users

---
+++ iam/iam_list_users.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+import boto3
+
+
+def list_iam_users():
+    iam = boto3.client('iam')
+
+    return iam.list_users()
+
+
+if __name__ == '__main__':
+    pprint(list_iam_users())
+    # for user in list_iam_users()['Users']:
+    #     print("User: {0}\nUserID: {1}\nARN: {2}\nCreatedOn: {3}\n".format(
+    #         user['UserName'],
+    #         user['UserId'],
+    #         user['Arn'],
+    #         user['CreateDate']
+    #         )
+    #     )
